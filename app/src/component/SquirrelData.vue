bb44e9e9766ea41efb2c2e8c4371f3f4bacf954d
Serena <serenazhou4268@gmail.com>
2026-03-16 12:31:32 -0400

    main

---
+++ app/src/component/SquirrelData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ SquirrelData.name }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const squirrel = ref(null)
+async function getSquirrel (id) {
+  const response = await fetch(`https://data.cityofnewyork.us/api/v3/views/vfnx-vebw/query.json`)
+  const data = await response.json()
+  pokemon.value = data
+}
+watch(
+  () => route.params.id,
+  function (id) {
+    getSquirrel(id)
+  },
+)
+onMounted(function () {
+  getSquirrel()
+})
+</script>
+
+<style scoped></style>
