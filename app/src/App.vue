163984001a7c1c4b6d3e9a00527d2dcc761361d6
Serena <serenazhou4268@gmail.com>
2026-03-24 12:27:41 -0400

    main

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h1>Squirrel</h1>
-
-    <RouterLink class="router__link" to="/vue-lists">Go to Squirrels</RouterLink>
+    <div class="router">
+      <RouterLink class="router__link" to="/vue-lists">NYC Squirrel Data</RouterLink>
+    </div>
     <RouterView />
+    <h1>Squirrel Data</h1>
   </div>
 </template>
 
@@ -15,8 +16,18 @@ h1 {
   text-align: center;
 }
 
-.router__link {
+div {
   font-family: 'Poppins', sans-serif;
   text-align: center;
+  align-items: center;
+}
+
+.router {
+  background-color: rgb(211, 201, 190);
+  margin: 2rem;
+}
+
+.router__link {
+  padding: 1rem;
 }
 </style>
